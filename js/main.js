3bcb221b7c31c7dcbe632a6fc3a52e85b873c3e3
McLensky <Dan@MacBook-Air.local>
2016-02-16 07:43:44 +1100

    after class

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+
+    //Score Keeper
+    var total = 0;
+
+    $('.add1').click(function () {
+        total = total + 1;
+        $('.score').text(total);
+    })
+
+    $('.add2').click(function () {
+        total = total + 2;
+        $('.score').text(total);
+    })
+
+    $('.add3').click(function () {
+        total = total + 3;
+        $('.score').text(total);
+    })
+
+    $('.sub1').click(function () {
+        total = total - 1;
+        $('.score').text(total);
+    })
+
+    $('.reset').click(function () {
+        total = 0;
+        $('.score').text(total);
+    })
+
+
+    //Compare That
+    $('.compare').click(function () {
+        //alert('hello');
+        var comparison;
+        var a = $('#value1').val(); //.val() gets value entered
+        var b = $('#value2').val(); //.val() gets value entered
+
+        if (a < b) {
+            comparison = '<';
+        } else if (a > b) {
+            comparison = '>';
+        } else if (a == b) {
+            comparison = '==';
+        } else {
+            comparison = 'N/A'
+        }
+
+        $('#comparison').text(comparison);
+    })
+
+    //Light Switch
+    var lights = "on";
+    $('.light-switch').click(function () {
+        if (lights == "on") {
+            $('#light-switch').attr('id', 'dark');
+            lights = "off";
+        } else {
+            $('#light-switch').attr('id', 'light-switch');
+            lights = "on";
+        }
+    })
+})
