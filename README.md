fcacdada57b215ca8497a2e392b35bd04986364f
Dan Pelensky <dan@pelensky.com>
2016-11-02 12:02:57 +0000

    readme added, as well as uncommited changes from february

---
+++ README.md
@@ -0,0 +1,5 @@
+# Front End Web Development
+## General Assembly
+### JS Fun
+
+First attempt using Javascript and jQuery to make a page interactive, coded along with instructor. 
--- js/main.js
+++ js/main.js
@@ -1,34 +1,50 @@
 $(document).ready(function () {
 
 
+
+
     //Score Keeper
+
     var total = 0;
 
-    $('.add1').click(function () {
-        total = total + 1;
-        $('.score').text(total);
-    })
+    function changeTotal(points) {
+        total = total + points;
+        return total;
+    }
+    $('.home button').click(function () {
+            total = total + parseInt($(this).text())
+            $('.score').text(total);
+        })
+        /*$('.add1').click(function () {
+                newTotal = changeTotal(1);
+                $('.score').text(newTotal);
+            })
+            
+                $('.add1').click(function () {
+                    total = total + 1;
+                    $('.score').text(total);
+                })
 
-    $('.add2').click(function () {
-        total = total + 2;
-        $('.score').text(total);
-    })
+                $('.add2').click(function () {
+                    total = total + 2;
+                    $('.score').text(total);
+                })
 
-    $('.add3').click(function () {
-        total = total + 3;
-        $('.score').text(total);
-    })
+                $('.add3').click(function () {
+                    total = total + 3;
+                    $('.score').text(total);
+                })
 
-    $('.sub1').click(function () {
-        total = total - 1;
-        $('.score').text(total);
-    })
+                $('.sub1').click(function () {
+                    total = total - 1;
+                    $('.score').text(total);
+                })
 
-    $('.reset').click(function () {
-        total = 0;
-        $('.score').text(total);
-    })
-
+                $('.reset').click(function () {
+                    total = 0;
+                    $('.score').text(total);
+                })
+            */
 
     //Compare That
     $('.compare').click(function () {
@@ -60,5 +76,15 @@ $(document).ready(function () {
             $('#light-switch').attr('id', 'light-switch');
             lights = "on";
         }
+    })
+
+    $('header ul li a').hover(function () {
+        $('.mega-menu').slideDown();
+    }, function () {
+        $('.mega-menu').slideUp();
+    })
+    
+    $('button.fadeIn').click(function(){
+        $(this).next().fadeToggle();
     })
 })
